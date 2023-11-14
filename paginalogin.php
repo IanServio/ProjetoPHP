409a31e41a455b137d9660314a88f3499c173665
IanServio <ianservioppe6@gmail.com>
2023-11-14 00:51:39 -0300

    arquivos front-end

--- paginalogin.php
+++ paginalogin.php
@@ -1,13 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+    <!--Botão para voltar para o inicio-->
+<a href="index.php" style="margin-right: 5px;
+     color: black;
+      font-family: Calibri;
+        padding: 5px;
+         border: 1px solid black;
+          background-color: red;
+           border-radius: 5px;
+           text-decoration: none;
+           ">Inicio</a>
+           <!--Fimm-->
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log-in </title>
+    <title>Log-in</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
         body{
-            background-color: #7e33b7;
+            background-color: #372162;
         }
         .login{
             width: 100%;
@@ -25,10 +38,10 @@
                 <div class="col-log-4 offset-lg-4">
                     <div class="card">
                         <div class="card-body">
-                            <h3>Log-in usuario</h3>
+                            <h3>Acesso Restrito</h3>
                         </div>
                         <div class="card-body">
-                            <form action="########" method="POST">
+                            <form action="login.php" method="POST">
                                 <div>
                                     <div class="mb-3">
                                         <label>Usuario</label>
--- topo.menu.php
+++ topo.menu.php
@@ -4,52 +4,34 @@
  display: flex;
    align-items: center;">
 
-  <img width="350" src="./png/logo3.png" alt="calibre" style="margin-right: 10px;">
+  <a href="index.php">
+    <img width="50" src="./png/logo-menu.png" alt="calibre" style="margin-right: 10px;">
+  </a>
 
-  <!--Menu de links-->
-  <a href="index.php" style="margin-right: 5px;
-     color: black;
-      font-family: Calibri;
-        padding: 5px;
-         border: 1px solid black;
-          background-color: #F0F8FF;
-           border-radius: 5px;
-           text-decoration: none;
-           ">INICIO</a>
+  <!--margin-right: 5px;         Margem à direita de 5 pixels 
+    color: black;                Cor do texto: preto 
+    font-family: Calibri;        Tipo de fonte: Calibri 
+    padding: 5px;                Preenchimento interno de 5 pixels 
+    border: 1px solid black;     Borda de 1 pixel sólido preto 
+    background-color: #F0F8FF;   Cor de fundo: #F0F8FF 
+    border-radius: 5px;          Raio da borda para criar cantos arredondados 
+    text-decoration: none;       Remover sublinhado padrão de link 
+    line-height: 15px;           Ajuste a altura da linha conforme necessário 
+    vertical-align: text-top;    Ajuste a posição vertical -->
 
-  <a href="?pg=quemsomos" style="margin-right: 5px;
-     color: black;
-      font-family: Calibri;
-        padding: 5px;
-         border: px solid black;
-          background-color: #F0F8FF;
-           border-radius: 5px;
-           text-decoration: none;
-           ">QUEM SOMOS</a>
+<!--Menu de links-->
+<a href="index.php"><img width="75" src="./png/inicio-icone.png" style="margin-bottom: -10; margin-right: 15px;"></a>
 
-  <a href="?pg=faleconosco" style="color: black;
-     font-family: Calibri;
-       padding: 5px;
-        border: 1px solid black;
-         background-color: #F0F8FF;
-          border-radius: 5px;
-          text-decoration: none;
-          ">FALE CONOSCO</a>
+<a href="?pg=faleconosco"><img width="75" src="./png/duvidas-icone.png" style="margin-bottom: -10; margin-right: 15px;"></a>
 
-  <!--Barra de pesquisa-->
-  <h3>
-  <div style="margin-left: 5px;">
-    <input type="text" placeholder="Pesquisar" style="padding: 8px;
-         border: 1px solid black;
-          border-radius: 5px;">
+<a href="?pg=camisas"><img width="75" src="./png/camisas-icone.png" style="margin-bottom: -10; margin-right: 15px;"></a>
 
-    <button style="color: black;
-         font-family: Calibri;
-           padding: 8px;
-            border: 1px solid black;
-             background-color: #84fa84;
-              border-radius: 5px;
-              ">Pesquisar</button>
-              </h3>
-  </div>
+
+  <!--Botãos de Login - registrar-->
+  <a href="paginalogin.php"><img width="35" src="./png/icone-login.png"
+  style="margin-bottom: -10;
+  margin-left: 900px;
+ ">
+  
+  </a>
 </div>
