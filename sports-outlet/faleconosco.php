06ae12ea8db529083ecd81d408b7805ce0cf8a73
Kayk Adriano <kaykadriano022@gmail.com>
2023-11-21 17:47:45 -0300

    Att

--- sports-outlet/faleconosco.php
+++ sports-outlet/faleconosco.php
@@ -1,10 +1,10 @@
 <div style="background-color: #372162;
-text-align: center;
-padding: 5px;
-border: 1px solid black;
-border-radius: 5px;
-max-width: 400px;
-margin: 10 auto;">
+    text-align: center;
+    padding: 5px;
+    border: 1px solid black;
+    border-radius: 5px;
+    max-width: 400px;
+    margin: 10 auto;">
     <h1 style="font-family: 'calibre', sans-serif;color:white">Fale Conosco</h1>
 
     <style>
@@ -16,26 +16,26 @@ margin: 10 auto;">
         <form action="?pg=sucesso" method="post">
             <label for="nome"><b>Nome</b></label><br>
             <input type="text" name="nome" id="nome" style="width: 100%;
-padding: 10px;
-margin-bottom: 10px;
-border: 1px solid black;
-border-radius: 3px;
-font-size: 16px;"><br>
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 1px solid black;
+    border-radius: 3px;
+    font-size: 16px;" required><br>
 
             <label for="email"><b>E-mail</b></label><br>
             <input type="text" name="email" id="email" style="width: 100%; padding: 10px;
-margin-bottom: 10px;
-border: 1px solid black;
-border-radius: 3px;
-font-size: 16px;"><br>
+    margin-bottom: 10px;
+    border: 1px solid black;
+    border-radius: 3px;
+    font-size: 16px;" required><br>
 
             <label for="assunto"><b>Assunto</b></label><br>
             <select name="assunto" id="assunto" style="width: 100%;
-padding: 10px;
-margin-bottom: 10px;
-border: 1px solid black;
-border-radius: 3px;
-font-size: 16px;">
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 1px solid black;
+    border-radius: 3px;
+    font-size: 16px;" required>
                 <style>
                     h4 {
                         font-family: 'calibre', sans-serif;
@@ -50,19 +50,19 @@ font-size: 16px;">
 
             <label for="mensagem"><b>Mensagem</b></label><br>
             <textarea name="mensagem" id="mensagem" style="width: 100%; padding: 10px;
-margin-bottom: 10px;
-border: 1px solid black;
-border-radius: 3px;
-font-size: 16px;
-height: 150px;"></textarea><br>
+    margin-bottom: 10px;
+    border: 1px solid black;
+    border-radius: 3px;
+    font-size: 16px;
+    height: 150px;" required></textarea><br>
 
             <input type="submit" value="Enviar" style="background-color: #84fa84;
-color: black;
-border: none;
-padding: 10px 20px;
-cursor: pointer;
-border-radius: 5px;
-font-size: 18px;">
+    color: black;
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+    border-radius: 5px;
+    font-size: 18px;">
     </h3>
     </form>
 </div>
