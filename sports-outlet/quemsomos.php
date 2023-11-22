1da652ca602fb4bd389b0332dc46eec63280021b
Vinicius Maciell <c4vinicius2711@gmail.com>
2023-11-22 07:56:37 -0300

    att

--- sports-outlet/quemsomos.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <center>
-        <h1 style="font-family: 'calibre', sans-serif;">!!!</h1>
-        <h3 style="font-family: 'calibre', sans-serif;">
-            Em Processo</h3>
-
-</div>
