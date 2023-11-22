95ef0ae9365d168c89c26f748ebb12df0bb2e195
IanServio <ianservioppe6@gmail.com>
2023-11-22 00:19:20 -0300

    Funcao Excluir OK

--- admin/salvarproduto.php
+++ admin/salvarproduto.php
@@ -44,6 +44,25 @@
                     echo "<script>location.href='?page=listarp'; </script>";
     
                 }
+            break;
+            case 'excluirp':
+                $titulo = $_POST["titulo"];
+                $imagem = $_POST["imagem"];
+                $fonte = $_POST["fonte"];
+        
+                $sql = "DELETE FROM produtos WHERE id={$_REQUEST['id']}";
+        
+        
+                $res = $conn->query($sql);
+        
+                if($res==true){
+                    echo "<script>alert ('Produto Excluido com sucesso'); </script>";
+                    echo "<script>location.href='?page=listarp'; </script>";
+                 }else{
+                    echo "<script>alert ('Nao foi possivel Excluir o Produto'); </script>";
+                     echo "<script>location.href='?page=listarp'; </script>";
+        
+                }
                 break;
         }
 
